65a9e9da6c9cdbbc62cc98620510e022dc44699f
IHateMint <neolunar7@snu.ac.kr>
2020-03-12 23:25:06 +0900

    spark setting added

--- mac/setting.sh
+++ mac/setting.sh
@@ -2,7 +2,12 @@
 brew install wget
 
 # Dev Tools
-
 brew tap AdoptOpenJDK/openjdk
 brew cask install adoptopenjdk8
 # java -version
+
+wget http://apache.tt.co.kr/spark/spark-2.4.5/spark-2.4.5-bin-hadoop2.7.tgz
+tar -xvzf ~/spark-2.4.5-bin-hadoop2.7.tgz
+mv ~/spark-2.4.5-bin-hadoop2.7 /opt/spark
+
+
