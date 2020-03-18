a6effef168e6c2c854e3bebc73df16051dfde38a
IHateMint <neolunar7@snu.ac.kr>
2020-03-18 22:26:04 +0900

    some changes

--- mac/setting.sh
+++ mac/setting.sh
@@ -10,4 +10,7 @@ wget http://apache.tt.co.kr/spark/spark-2.4.5/spark-2.4.5-bin-hadoop2.7.tgz
 tar -xvzf ~/spark-2.4.5-bin-hadoop2.7.tgz
 mv ~/spark-2.4.5-bin-hadoop2.7 /opt/spark
 
+## Useful Tools
+curl -sfL https://direnv.net/install.sh | bash # direnv -> to .bashrc or .zshrc, add -> eval "$(direnv hook bash)"
+brew install the_silver_searcher # ag (Better than grep)
 
